5209d6acc536063ca0d7e87ddb041efdcfc3f534
Tamer Essam <69662300+TamerJarrar@users.noreply.github.com>
2022-07-13 18:02:37 +0400

    News Screen With React Native

---
+++ pages/news/index.js
@@ -0,0 +1,127 @@
+import { View, Text, SafeAreaView, FlatList } from "react-native-web";
+import ReactPlayer from 'react-player';
+
+
+const DATA = [
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+
+  {
+    id: 1,
+    name: "title",
+    desc: "desc",
+  },
+];
+const FilterData = [
+  {
+    id: 1,
+    title: "English",
+  },
+  {
+    id: 2,
+    title: "Hindi",
+  },
+  {
+    id: 3,
+    title: "Malayalam",
+  },
+  {
+    id: 3,
+    title: "Malayalam",
+  },
+  {
+    id: 3,
+    title: "Malayalam",
+  },
+];
+
+const index = () => {
+  return (
+    <View>
+      <SafeAreaView>
+        <View>
+          <FlatList
+            data={FilterData}
+            horizontal
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+    
+              <View style={{
+                marginHorizontal: 10,
+                marginVertical: 20
+              }}>
+              <View style={{
+                background: "#cbd5e0",
+                
+                borderRadius: 20
+              }}>
+              <Text style={{
+                padding: "15px",
+               
+              }}>{item.title}</Text>
+
+                </View>
+              </View>
+          
+            )}
+          />
+        </View>
+        <View>
+          <FlatList
+            data={DATA}
+            vertical
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <>
+                <View
+                  style={{
+                    marginVertical: 10,
+                  }}
+                >
+                    <View>
+                    <ReactPlayer width={"100%"} url={"https://www.youtube.com/watch?v=HdscSv53dGI"} />
+                    </View>
+                </View>
+              </>
+            )}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default index;
