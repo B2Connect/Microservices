83e54e870b9056a9f37e3c31613cf9f78b1181de
Tamer Essam <69662300+TamerJarrar@users.noreply.github.com>
2022-07-20 11:43:07 +0400

    Liner

--- pages/meditation/[id].js
+++ pages/meditation/[id].js
@@ -8,7 +8,7 @@ const Post = () => {
   return (
         <ScrollView style={{
             background: "rgb(255,57,159)",
-            background: "linear-gradient(360deg, rgba(227,0,101,1) 0%, rgba(238,0,122,0.7931547619047619) 49%, rgba(238,0,122,0.8491771708683473) 50%, rgba(227,0,101,0.7343312324929971) 100%)",
+            background: "linear-gradient(352deg, rgba(227,0,101,1) 0%, rgba(228,0,102,0.7399334733893557) 51%, rgba(227,0,101,0.711922268907563) 100%)",
             minHeight: "100vh"
         }}>
             <h2>This is Tamer</h2>
