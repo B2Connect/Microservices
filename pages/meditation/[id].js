fdbbbfa33cfd20309aed5cdd886386f6d5c606c5
Tamer Essam <69662300+TamerJarrar@users.noreply.github.com>
2022-07-20 12:14:39 +0400

    Animation

--- pages/meditation/[id].js
+++ pages/meditation/[id].js
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
-import { ScrollView } from 'react-native-web'
+import { ScrollView, View } from 'react-native-web'
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const Post = () => {
   const router = useRouter()
@@ -9,9 +10,29 @@ const Post = () => {
         <ScrollView style={{
             background: "#0C2340",
             background: "#0C2340",
-            minHeight: "100vh"
+            minHeight: "100vh",
         }}>
             <h2>This is Tamer</h2>
+            <View style={{
+                display: "flex",
+                flexDirection: "center",
+                alignItems:"center"
+
+            }}>
+                 <Player
+                    autoplay    
+                    loop
+                    src={
+                      "https://assets3.lottiefiles.com/packages/lf20_72GGiU.json"
+                    }
+                    style={{
+                      height: "200px",
+                      width: "200px",
+                      marginRight: 5,
+                    }}
+                  ></Player>
+            </View>
+           
         </ScrollView>
   )
 }   
