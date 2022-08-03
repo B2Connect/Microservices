956b9ec6f3d52f4351f854577ee246100ac239a2
Tamer Essam <69662300+TamerJarrar@users.noreply.github.com>
2022-08-03 11:29:09 +0400

    Workout Loader Update

--- pages/workout/index.js
+++ pages/workout/index.js
@@ -80,35 +80,37 @@ const index = () => {
         />
       </div>
     );
+  }else{
+    return (
+      <main>
+        <div class="muscle-groups">
+          <View style={{display: "flex",  alignItems: "center", marginVertical: 50}}>
+            <Text style={{fontSize: 22, fontWeight: 600}}>Select A Muscle To Train</Text>
+          </View>
+          <div style={{
+            alignSelf: "center"
+          }}>
+    <ReactSVG
+            src={"/images/body.svg"}
+            onClick={(e) => {
+              handleMuscle(e)
+            }}
+          />
+          <View style={{alignItems: "center", marginVertical: 20}}>
+            <Text>OR</Text>
+          </View>
+          <View style={{marginVertical: 20}}>
+            <Button color="#e0457c" title="Full body workout" onPress={handleFullWorkout} />
+  
+          </View>
+          </div>
+        
+        </div>
+      </main>
+    );
   }
  
 
-  return (
-    <main>
-      <div class="muscle-groups">
-        <View style={{display: "flex",  alignItems: "center", marginVertical: 50}}>
-          <Text style={{fontSize: 22, fontWeight: 600}}>Select A Muscle To Train</Text>
-        </View>
-        <div style={{
-          alignSelf: "center"
-        }}>
-  <ReactSVG
-          src={"/images/body.svg"}
-          onClick={(e) => {
-            handleMuscle(e)
-          }}
-        />
-        <View style={{alignItems: "center", marginVertical: 20}}>
-          <Text>OR</Text>
-        </View>
-        <View style={{marginVertical: 20}}>
-          <Button color="#e0457c" title="Full body workout" onPress={handleFullWorkout} />
 
-        </View>
-        </div>
-      
-      </div>
-    </main>
-  );
 };
 export default index;
